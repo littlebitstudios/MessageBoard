b917e2e5fa83b544a76df21dec5045cff33700a6
jaherron <littlebit@littlebitstudios.com>
2024-08-13 00:35:28 -0500

    chore: Refactor post container to sort posts by creation date (new to old)

--- app/postcontainer.jsx
+++ app/postcontainer.jsx
@@ -23,31 +23,33 @@ export default function PostContainer() {
     <>
       <button onClick={fetchPosts}>Reload Posts</button>
       <a href="/post" className='button'>Write a Post</a>
-      {posts.map((post) => {
-        const formattedDateTime = new Date(post.createdAt).toLocaleString('en-US', {
-          year: '2-digit',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          hour12: true,
-        });
-  
-        return (
-          <div key={post.id} className="post">
-            <div className="profile-container">
-              <img src={post.profilePicture} />
-              <div className="text-container">
-                <strong>{post.displayname}</strong>
-                <br />@{post.username} - posted on {formattedDateTime}
-              </div>
+      {posts
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    .map((post) => {
+      const formattedDateTime = new Date(post.createdAt).toLocaleString('en-US', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true,
+      });
+
+      return (
+        <div key={post.id} className="post">
+          <div className="profile-container">
+            <img src={post.profilePicture} />
+            <div className="text-container">
+              <strong>{post.displayname}</strong>
+              <br />@{post.username} - posted on {formattedDateTime}
             </div>
-            <p>{post.content}</p>
-            <DeleteButton id={post.id} fetchFunction={fetchPosts}/>
-            <a href={`/post/${post.id}`} className='button'>Edit</a>
           </div>
-        );
-      })}
+          <p>{post.content}</p>
+          <DeleteButton id={post.id} fetchFunction={fetchPosts}/>
+          <a href={`/post/${post.id}`} className='button'>Edit</a>
+        </div>
+      );
+    })}
     </>
   );
 }
