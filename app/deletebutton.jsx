ca9958ace683c00fc2ff9d91075012213545f2c1
jaherron <littlebit@littlebitstudios.com>
2024-08-20 03:21:37 -0500

    ensuring posts can only be deleted by their owner

--- app/deletebutton.jsx
+++ app/deletebutton.jsx
@@ -1,7 +1,24 @@
 "use client";
 
+import { useState } from 'react';
+
 export default function DeleteButton({id, fetchFunction}) {
+    const [authorProfileId, setAuthorProfileId] = useState('');
+
     async function deletePost() {
+        fetch(`/api/posts/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setAuthorProfileId(data.profileId);
+            });
+
+        if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+            if (authorProfileId !== localStorage.getItem('defaultProfileId')) {
+                alert('You cannot delete this post, you are not its author. If you are the author, go to the Profile Manager and choose the correct profile.');
+                return;
+            }
+        }
+
         await fetch(`/api/posts/${id}`, {
             method: 'DELETE',
         });
